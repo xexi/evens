9d3248fb92dbbfd6d598bb2139bea4da167f15ae
Hyper <synth9@naver.com>
2017-10-15 09:34:04 -0400

    Add travis

---
+++ test/test.js
@@ -0,0 +1,11 @@
+"use strict";
+// test unavailable due to time issue
+var expect = require('chai').expect;
+
+describe('Math', function() {  
+    describe('#abs()', function() {
+        it('should return positive value of given negative number', function() {
+            expect(Math.abs(-5)).to.be.equal(5);
+        });
+    });
+});
